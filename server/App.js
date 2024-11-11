2127800571ffc8945bead20ea9dfff52cfee09ba
Reshab Naskar <naskarreshab2003@gmail.com>
2024-11-11 19:24:10 +0530

    uno

--- server/App.js
+++ server/App.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-    origin: 'https://task-management-app-2024.web.app/',
+    origin: 'https://task-management-app-2024.web.app/register',
     credentials: true,
 };
 
